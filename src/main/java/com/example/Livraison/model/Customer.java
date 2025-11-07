915908ae61697e4b660a0001e0186bc1a918f36e
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-11-07 16:27:08 +0100

    Add Customer entity with fields and relationships for deliveries

---
+++ src/main/java/com/example/Livraison/model/Customer.java
@@ -0,0 +1,35 @@
+import com.example.Livraison.model.Delivery;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    private double latitude;
+    private double longitude;
+
+    private String preferredTimeSlot;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Delivery> deliveries;
+}
