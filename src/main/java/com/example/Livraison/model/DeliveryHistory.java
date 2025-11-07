ccaecd6798229f69b59f8203cb66c4c644e8da3f
MohamedHanafiAlaoui <hanafimohamed510@gmail.com>
2025-11-07 19:41:07 +0100

    Add DeliveryHistory entity with fields for delivery tracking and relationships to Delivery, Customer, and Tour

---
+++ src/main/java/com/example/Livraison/model/DeliveryHistory.java
@@ -0,0 +1,47 @@
+package com.example.Livraison.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "delivery_history")
+public class DeliveryHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.DATE)
+    private Date deliveryDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date plannedTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date actualTime;
+
+    private long delay;
+    private String dayOfWeek;
+
+    @ManyToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "tour_id")
+    private Tour tour;
+}
